86cf3f94fdfd80a863fcebfaf96734fdfd08397e
Jan Verhagen <verhagen.jca@gmail.com>
2015-08-13 14:16:05 +0200

    Expanded test with column size (column number)

--- xlsx_test.py
+++ xlsx_test.py
@@ -8,6 +8,7 @@ sheet2 = document.addSheet()
 sheet1.addData('A2', '200', type='number')
 sheet1.addData('A3', '200', type='number')
 
+sheet1.setColumnSize(4, 20.2)
 sheet1.addData('D2', '100')
 sheet1.addData('D3', '120')
 sheet1.addData('D4', '120')
@@ -15,5 +16,8 @@ sheet1.addData('D4', '120')
 sheet1.addData('D5', 'SUM(D2:D4)', type='formule')
 
 sheet1.addData('E7', 'tekst', type='text')
+
+sheet2.addData('A2', '200', type='number')
+sheet2.addData('A3', '200', type='number')
 
 document.save('test_excel1.xlsx')
